bbcfc8482c743d6dd52eb9e50b6eaac3d9da6fce
Abhijit Kar <ak24284@gmail.com>
2017-12-25 11:19:11 +0530

    Salary and Highest Salary linked

--- src/app/features/game-view/services/salary.service.ts
+++ src/app/features/game-view/services/salary.service.ts
@@ -4,18 +4,23 @@ import { Injectable, EventEmitter } from "@angular/core";
 export class SalaryService {
     salaryUpdate: EventEmitter<number> = new EventEmitter<number>();
 
-    private _salary = 1000;
+    private _salary: number = 0;
     get salary(): string {
         return this._salary.toLocaleString();
     }
 
-    private _highestSalary = 10000;
+    private _highestSalary: number = 0;
     get highestSalary(): string {
         return this._highestSalary.toLocaleString();
     }
 
     updateSalary(salary: number) {
         this._salary += salary;
+
+        if(this._salary > this._highestSalary) {
+            this._highestSalary = this._salary;
+        }
+        
         this.salaryUpdate.emit(salary);
     }
 }
