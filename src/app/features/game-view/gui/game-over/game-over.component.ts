c90ba52f94626e1f63739b840d5db86ec129d20c
Abhijit Kar <ak24284@gmail.com>
2017-12-26 10:15:29 +0530

    Game Over screen completed

--- src/app/features/game-view/gui/game-over/game-over.component.ts
+++ src/app/features/game-view/gui/game-over/game-over.component.ts
@@ -1,13 +1,17 @@
 import { Component } from "@angular/core";
 import { ChoreographerService } from "../../services/choreographer/choreographer.service";
 import { GAME_STATES } from "../../services/choreographer/choreographer.model";
+import { SalaryService } from "../../services/salary.service";
 @Component({
   selector: "dlp-game-over",
   templateUrl: "./game-over.component.html",
   styleUrls: ["./game-over.component.css"]
 })
 export class GameOverComponent {
-    game_states = GAME_STATES;
-    
-    constructor(public choreographerService: ChoreographerService){}
+  game_states = GAME_STATES;
+
+  constructor(
+    public choreographerService: ChoreographerService,
+    public salaryService: SalaryService
+  ) {}
 }
