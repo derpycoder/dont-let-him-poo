eb16692ea4aea68d4a9a42be76b6fb657f22ba30
Abhijit Kar <ak24284@gmail.com>
2017-12-26 09:35:35 +0530

    Trash can selection works based on game state

--- src/app/features/game-view/gui/trash-can/trash-can.component.ts
+++ src/app/features/game-view/gui/trash-can/trash-can.component.ts
@@ -1,26 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { TILE_TYPES } from "../../services/";
 import { InteractionService } from "../../services/interaction.service";
+import { ChoreographerService } from "../../services/choreographer/choreographer.service";
+import { GAME_STATES } from "../../services/choreographer/choreographer.model";
 
 @Component({
   selector: "dlp-trash-can",
   templateUrl: "./trash-can.component.html",
   styleUrls: ["./trash-can.component.css"]
 })
-export class TrashCanComponent {
+export class TrashCanComponent implements OnInit {
   tile_types = TILE_TYPES;
 
-  constructor(public interactionService: InteractionService) {}
+  deactivateImage: boolean = true;
+
+  constructor(
+    public interactionService: InteractionService,
+    public choreographerService: ChoreographerService
+  ) {}
+
+  ngOnInit() {
+    this.choreographerService.onGameStateChange.subscribe(
+      (state: GAME_STATES) => {
+        this.deactivateImage = state !== GAME_STATES.RUNNING;
+        
+        if (state === GAME_STATES.START) {
+          this.interactionService.remainingQuantity.money = this.interactionService.remainingQuantity.pizza = 5;
+          this.interactionService.selectedTileType = TILE_TYPES.NONE;
+        }
+      }
+    );
+  }
 
   selectMoney() {
-    if (this.interactionService.remainingQuantity[TILE_TYPES.MONEY] > 0) {
+    if (this.choreographerService.currentGameState === GAME_STATES.RUNNING && this.interactionService.remainingQuantity[TILE_TYPES.MONEY] > 0) {
       this.interactionService.selectedTileType = TILE_TYPES.MONEY;
     }
   }
 
   selectPizza() {
-    if (this.interactionService.remainingQuantity[TILE_TYPES.PIZZA] > 0) {
+    if (this.choreographerService.currentGameState === GAME_STATES.RUNNING && this.interactionService.remainingQuantity[TILE_TYPES.PIZZA] > 0) {
       this.interactionService.selectedTileType = TILE_TYPES.PIZZA;
     }
   }
