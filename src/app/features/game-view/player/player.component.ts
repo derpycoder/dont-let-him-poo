be2748cdacf48226a859beb63382fee5b986e7a7
Abhijit Kar <ak24284@gmail.com>
2017-12-28 09:39:28 +0530

    Abrupt stopping of player on game over removed

--- src/app/features/game-view/player/player.component.ts
+++ src/app/features/game-view/player/player.component.ts
@@ -100,9 +100,6 @@ export class PlayerComponent implements OnInit {
           this.playerType = PLAYER_TYPES.HAPPY;
           this.animatePlayer();
           break;
-        case GAME_STATES.GAME_OVER:
-          this.tl.clear();
-          break;
         default:
       }
     });
