f0a6b17d80bcb4cdf717dee17ea75858ac22d533
Abhijit Kar <ak24284@gmail.com>
2017-12-29 18:57:35 +0530

    Progressive Web App Enablement
    1. Added many images, and renamed many
    2. Added manifest
    3. Changed normal dictionary of TILE_TYPES to enum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from "@angular/core";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-// App Root
 import { AppComponent } from "./app.component";
 
-// Feature Modules
 import { FeaturesModule } from "./features/features.module";
-
-// Routing Modules
 import { AppRoutingModule } from "./app.routing";
 
 @NgModule({
--- src/app/features/game-view/services/grid/grid.model.ts
+++ src/app/features/game-view/services/grid/grid.model.ts
@@ -1,16 +1,16 @@
 import { EventEmitter } from "@angular/core";
 
-export const TILE_TYPES = {
-  WALL: "wall",
-  POOP: "poop",
-  NONE: "none",
-  LOO: "loo",
+export enum TILE_TYPES {
+  WALL = "wall",
+  POOP = "poop",
+  NONE = "none",
+  LOO = "loo",
 
-  PIZZA: "pizza",
-  MONEY: "money",
+  PIZZA = "pizza",
+  MONEY = "money",
 
-  PLAYER: "player"
-};
+  PLAYER = "player"
+}
 
 export class Node {
   x: number;
