8b6cfaf12346b8f09faf9c5d120d4bb66b123497
Abhijit Kar <ak24284@gmail.com>
2017-12-30 19:19:32 +0530

    Tweet button added

--- src/app/features/game-view/game-view.component.ts
+++ src/app/features/game-view/game-view.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  PLATFORM_ID,
+  Inject
+} from "@angular/core";
 import { Router } from "@angular/router";
+
+import { isPlatformBrowser, isPlatformServer } from "@angular/common";
 
 import { environment } from "../../../environments/environment";
 
@@ -12,13 +20,78 @@ import { GAME_STATES } from "./services/choreographer/choreographer.model";
   styleUrls: ["./game-view.component.css"]
 })
 export class GameViewComponent {
+  tweetIntent: string = "https://twitter.com/intent/tweet/";
+
   game_states = GAME_STATES;
   env = environment;
 
   constructor(
     public choreographerService: ChoreographerService,
-    private router: Router
-  ) {}
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.constructTweetIntent();
+  }
+
+  private constructTweetIntent() {
+    const attributes = {
+      "?original_referer": "http://www.abhijit-kar.com",
+      "&text":
+        "Prevent the pesky emoji from going to loo. Coz, it's a game revolving around poo!",
+      "&hashtags": "dontletim",
+      "&url": "http://www.abhijit-kar.com/dont-let-him-poo/",
+      "&via": "ak24284"
+    };
+
+    Object.keys(attributes).forEach(key => {
+      this.tweetIntent += key + "=" + encodeURIComponent(attributes[key]);
+    });
+  }
+
+  tweet() {
+    if (isPlatformBrowser) {
+      this.popUpCenter(this.tweetIntent, "Share a link on Twitter", 500, 500);
+    }
+  }
+
+  private popUpCenter(url, title, w, h) {
+    if (isPlatformBrowser(this.platformId)) {
+      var dualScreenLeft =
+        window.screenLeft != undefined ? window.screenLeft : screen.left;
+      var dualScreenTop =
+        window.screenTop != undefined ? window.screenTop : screen.top;
+
+      var width = window.innerWidth
+        ? window.innerWidth
+        : document.documentElement.clientWidth
+          ? document.documentElement.clientWidth
+          : screen.width;
+      var height = window.innerHeight
+        ? window.innerHeight
+        : document.documentElement.clientHeight
+          ? document.documentElement.clientHeight
+          : screen.height;
+
+      var left = width / 2 - w / 2 + dualScreenLeft;
+      var top = height / 2 - h / 2 + dualScreenTop;
+      var newWindow = window.open(
+        url,
+        title,
+        "scrollbars=yes, width=" +
+          w +
+          ", height=" +
+          h +
+          ", top=" +
+          top +
+          ", left=" +
+          left
+      );
+
+      if (window.focus) {
+        newWindow.focus();
+      }
+    }
+  }
 
   showRankingsPage() {
     this.choreographerService.currentGameState = GAME_STATES.LOAD;
